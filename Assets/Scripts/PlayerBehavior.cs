ac85569a3ce1b2e0b4f625636d2ba3618af34f54
Robert Lafferty <rclafferty@ksu.edu>
2018-02-10 03:45:57 -0600

    Added Personal Tweaks
    Finalized controller functionality
    
    Added torch to player

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -3,36 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour {
-
-    Rigidbody body;
-
-    [SerializeField]
-    float forwardInput;
-    [SerializeField]
-    float sideInput;
-
+    
 	// Use this for initialization
 	void Start () {
-        body = this.GetComponent<Rigidbody>();
-
-        forwardInput = 0f;
-        sideInput = 0f;
+        
 	}
 
     // Update is called once per frame
     void Update() {
-        forwardInput = Input.GetAxis("Forward");
-        if (Mathf.Abs(forwardInput) > 0)
-        {
-            body.MovePosition(body.position + (Vector3.forward * -1 * forwardInput));
-            //body.AddRelativeForce(Vector3.forward);
-            //body.AddForce(Vector3.forward);
-        }
+        
+    }
 
-        sideInput = Input.GetAxis("Sideways");
-        if (Mathf.Abs(sideInput) > 0)
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.name == "COOL_Terrain" || collision.collider.name == "Terrain")
+            return;
+
+        Debug.Log("Collided with " + collision.collider.name);
+        if (collision.collider.name == "Door" && Input.GetAxis("Fire") > 0)
         {
-            body.MovePosition(body.position + (Vector3.right * forwardInput));
+            Debug.Log("Destroying " + collision.collider.name);
+            BurnDoor(collision);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    void BurnDoor(Collision collision)
+    {
+        Debug.Log("Burning " + collision.collider.name);
+        collision.collider.GetComponent<DoorBehavior>().Burn();
     }
 }
