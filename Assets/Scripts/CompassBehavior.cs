2d4f77def3b302deedbd1621a94379fdc4f0b150
Robert Lafferty <rclafferty@ksu.edu>
2018-02-10 06:11:01 -0600

    Update

---
+++ Assets/Scripts/CompassBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassBehavior : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(this.transform.rotation.eulerAngles.y);
+
+        this.transform.rotation = Quaternion.Euler(new Vector3(player.transform.eulerAngles.x, player.transform.eulerAngles.y, player.transform.eulerAngles.y));
+        
+	}
+}
