8a282acb1f0f218ddcaab2223a2e873e2d02ed68
Daniel Shchur <daniel.shchur@huskers.unl.edu>
2018-02-11 04:13:29 -0600

    Imported elements from Riot

---
+++ Assets/UI/NeedleAnimate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedleAnimate : MonoBehaviour {
+
+    public GameObject player;
+   //public RectTransform needle;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.Find("RigidBodyFPSController");
+        //needle = GetComponent<RectTransform>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //this.transform.rotation = Quaternion.Euler(player.transform.eulerAngles.x, player.transform.eulerAngles.y, player.transform.eulerAngles.y);
+        this.transform.rotation = Quaternion.Euler(0, 0, player.transform.eulerAngles.y);
+
+        //this.rotation = Quaternion.Euler(player.transform., 0, 1);
+
+    }
+}
